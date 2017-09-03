cba068be177c4216d22f51887255e969ac07b61e
dkush22 <dkush22@gmail.com>
2017-09-03 16:02:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function iterativeLog(array) {
-array.forEach((element, index) => console.log(`${index}: ${element}`))
+	array.forEach((element, index) => console.log(`${index}: ${element}`))
 }
 
 function iterate(callback) {
-	let array = [22, 25, 29]
-	array.forEach(callback)
-	return array
+	let nameArray = ["daniel", "liz"]
+	nameArray.forEach(element => callback(element))
+	return nameArray
 }
 
 function doToArray(array, callback) {
-	array.forEach(callback)
+	array.forEach(element => callback(element))
 }
